71886d7fd5d1f89815ac6931f4e9e0d8705698c5
Slava Tutrinov <slava@tutrinov.pro>
2015-07-06 21:59:15 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,51 @@
+import tornado.ioloop
+import tornado.web
+import socket_handler
+
+from tornado import options, websocket
+
+options.define("port", default=8888, help="run on the given port", type=int)
+options.define("debug", default=False, help="run in debug mode")
+
+clients = []
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(request):
+        request.render('index.html')
+
+class SocketHandler(websocket.WebSocketHandler):
+
+    def on_message(self, message):
+        message = tornado.escape.json_encode({
+            'text': message
+        })
+        for cl in clients:
+            cl.write_message(message)
+
+    def open(self, *args, **kwargs):
+        clients.append(self)
+        print("WebSocket opened")
+
+    def on_close(self):
+        clients.remove(self)
+        print("WebSocket closed")
+
+
+def main():
+    options.parse_command_line()
+    opts = options.options
+    application = tornado.web.Application(
+        [
+            (r"/", IndexHandler),
+            (r"/ws", SocketHandler),
+            (r"/ws/timer", socket_handler.WorkTimerHandler),
+        ],
+        debug=opts.debug
+    )
+    application.listen(opts.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ socket_handler.py
@@ -0,0 +1,96 @@
+from tornado import websocket
+from tornado import escape
+
+import datetime as dt
+import pickle
+
+clients = []
+descriptors = {}
+
+class TaskDescriptor:
+
+    STATUS_INITED = 1
+    STATUS_RESETED = 2
+    STATUS_STARTED = 3
+    STATUS_STOPED = 4
+
+    def __init__(self, task_name, ext='.pkl', path='./task_descriptors'):
+        self.ext = ext
+        self.path = path
+        self.task_name = task_name
+        self.task_descriptor_file_path = self.path + self.task_name + self.ext
+        self.set_status(self.STATUS_INITED)
+
+    def task_init(self, task):
+        f = open(self.task_descriptor_file_path, 'wb')
+        work = {
+            'result': [],
+            'timer': []
+        }
+        pickle.dump(work, f)
+        self.set_status(self.STATUS_INITED)
+
+    def task_reset(self, task):
+        work = pickle.load(open(self.task_descriptor_file_path, 'rb'))
+        work['result'] = []
+        work['timer'] = []
+        pickle.dump(work, open(self.task_descriptor_file_path, 'wb'))
+        self.set_status(self.STATUS_RESETED)
+
+    def task_start(self, task):
+        work = pickle.load(open(self.task_descriptor_file_path, 'rb'))
+        work['timer'].append(dt.datetime.now())
+        pickle.dump(work, open(self.task_descriptor_file_path, 'wb'))
+        self.set_status(self.STATUS_STARTED)
+
+    def task_stop(self, task):
+        work = pickle.load(open(self.task_descriptor_file_path, 'rb'))
+        stop_time = dt.datetime.now()
+        delta = (stop_time - work['timer'].pop()).total_seconds()
+        work['result'].append(delta/3600)
+        pickle.dump(work, open(self.task_descriptor_file_path, 'wb'))
+        self.set_status(self.STATUS_STOPED)
+
+    def set_status(self, status):
+        self.status = status
+
+    def get_status(self):
+        return self.status
+
+class WorkTimerHandler(websocket.WebSocketHandler):
+    def on_close(self):
+        clients.remove(self)
+        print("WebSocket closed")
+
+    def open(self, *args, **kwargs):
+        clients.append(self)
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        client_request_data = escape.json_decode(message)
+        task_name = client_request_data.task
+        if (task_name not in descriptors):
+            descriptors[task_name] = TaskDescriptor(task_name)
+        """
+            TaskDescriptor descriptor
+        """
+        descriptor = descriptors[task_name]
+        try:
+            actions = {
+                "reset": lambda task: descriptor.task_reset(task),
+                "start": lambda task: descriptor.task_start(task),
+                "stop": lambda task: descriptor.task_stop(task),
+                "init": lambda task: descriptor.task_init(task)
+            }
+            for client in clients:
+                client.write_message(escape.json_encode({
+                    'task': task_name,
+                    'status': descriptor.get_status()
+                }))
+        except Exception as ex:
+            for client in clients:
+                client.write_message(escape.json_encode({
+                    'error': ex.message
+                }))
+            pass
+        actions[client_request_data.action](client_request_data.task)
